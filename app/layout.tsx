1e86b2d8c9098eec7e78e6dc37137d6620fc1e62
parvin-noori <parvinnoori0@gmail.com>
2026-07-13 11:45:03 +0330

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import Footer from "./_components/layout/footer/footer";
+import Header from "./_components/layout/header/header";
+import NavigationBar from "./_components/navigationBar/NavigationBar";
 import { Peyda } from "./fonts";
 import "./globals.css";
 
@@ -8,7 +11,12 @@ export default function RootLayout({
 }>) {
   return (
     <html dir="rtl" lang="fa" className={` ${Peyda.className}  h-full`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Header />
+        {children}
+        <Footer />
+        <NavigationBar />
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,6 @@ import Posts from "./_components/blog/posts";
 import Categories from "./_components/categories/categories";
 import CustomBouquet from "./_components/customBouquet/customBouquet";
 import Hero from "./_components/hero/Hero";
-import Footer from "./_components/layout/footer/footer";
-import Header from "./_components/layout/header/header";
-import NavigationBar from "./_components/navigationBar/NavigationBar";
 import ProductCarousel from "./_components/products/ProductCarousel";
 import Reviews from "./_components/reviews/reviews";
 import Services from "./_components/services/services";
@@ -13,25 +10,22 @@ import Services from "./_components/services/services";
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
       <div className="overflow-hidden">
         <div className="container flex flex-col divide-y divide-gray-200 gap-y-10">
           <Categories />
           <Services />
           <div className=" flex flex-col  gap-y-10">
-            <ProductCarousel isOffer title="تخفیفات شگفت انگیز"/>
+            <ProductCarousel isOffer title="تخفیفات شگفت انگیز" />
             <ProductCarousel title="پرفروش ترین ها" />
-            <CustomBouquetBanners/>
+            <CustomBouquetBanners />
             <ProductCarousel title="روز دختر" />
           </div>
-          <CustomBouquet/>
-          <Reviews/>
-          <Posts/>
+          <CustomBouquet />
+          <Reviews />
+          <Posts />
         </div>
       </div>
-      <Footer/>
-      <NavigationBar />
     </>
   );
 }
