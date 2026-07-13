da2a6599e6c5ceb530eaf4288b6e9c9bec6322f7
parvin-noori <parvinnoori0@gmail.com>
2026-07-13 10:05:27 +0330

    change font

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,47 +3,47 @@ import localFont from "next/font/local";
 export const Peyda = localFont({
   src: [
     {
-      path: "./fonts/PeydaWeb-Thin.woff",
+      path: "./fonts/PeydaWeb-Thin(FaNum).woff2",
       weight: "100",
       style: "normal",
     },
     {
-      path: "./fonts/peydaWeb-extralight.woff",
+      path: "./fonts/PeydaWeb-ExtraLight(FaNum).woff2",
       weight: "200",
       style: "normal",
     },
     {
-      path: "./fonts/peydaWeb-light.woff",
+      path: "./fonts/PeydaWeb-Light(FaNum).woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "./fonts/PeydaWeb-Regular.woff",
+      path: "./fonts/PeydaWeb-Regular(FaNum).woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts/PeydaWeb-Medium.woff",
+      path: "./fonts/PeydaWeb-Medium(FaNum).woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./fonts/PeydaWeb-SemiBold.woff",
+      path: "./fonts/PeydaWeb-SemiBold(FaNum).woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "./fonts/PeydaWeb-Bold.woff",
+      path: "./fonts/PeydaWeb-Bold(FaNum).woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "./fonts/PeydaWeb-ExtraBold.woff",
+      path: "./fonts/PeydaWeb-ExtraBold(FaNum).woff2",
       weight: "800",
       style: "normal",
     },
     {
-      path: "./fonts/PeydaWeb-Black.woff",
+      path: "./fonts/PeydaWeb-Black(FaNum).woff2",
       weight: "900",
       style: "normal",
     },
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html dir="rtl" lang="en" className={` ${Peyda.className}  h-full`}>
+    <html dir="rtl" lang="fa" className={` ${Peyda.className}  h-full`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
