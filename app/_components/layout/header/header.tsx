d78e626086b8fe99e0bdddedcbc0691096979339
parvin-noori <parvinnoori0@gmail.com>
2026-07-13 11:29:42 +0330

    update header

--- app/_components/layout/header/header.tsx
+++ app/_components/layout/header/header.tsx
@@ -65,13 +65,13 @@ export default function Header() {
               action=""
               className="searchbar flex items-center bg-gray-100 text-gray-400 rounded-full overflow-hidden text-sm flex-grow"
             >
-              <button type="submit" className="p-4  cursor-pointer">
+              <button type="submit" className="lg:p-4 p-3 cursor-pointer">
                 <Search />
               </button>
               <input
                 type="text"
                 placeholder="جستجوی گل در شکوفا"
-                className="outline-none w-full"
+                className="outline-none w-full lg:py-4 py-3"
               />
             </Form>
           </div>
