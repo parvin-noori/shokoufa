5589b001ade8a6115a7f4f67c134a80664127be4
parvin-noori <parvinnoori0@gmail.com>
2026-07-14 09:14:03 +0330

    update navigationbar

--- app/_components/navigationBar/NavigationBar.jsx
+++ app/_components/navigationBar/NavigationBar.jsx
@@ -1,44 +1,60 @@
+"use client";
+
 import { Heart, House, ShoppingCart, User } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cloneElement } from "react";
 
 export default function NavigationBar() {
   const navigationMenus = [
     {
       title: "خانه",
       icon: <House />,
-      link: "/",
+      href: "/",
     },
     {
       title: "سبد خرید",
       icon: <ShoppingCart />,
-      link: "/",
+      href: "/cart",
     },
     {
       title: "علاقه مندی ها",
       icon: <Heart />,
-      link: "/",
+      href: "/favorite",
     },
     {
       title: "پروفایل",
       icon: <User />,
-      link: "/",
+      href: "/user",
     },
   ];
+
+  const pathName = usePathname();
   return (
     <div className="fixed bottom-0 bg-white inset-x-0 py-4 inset-shadow-sm lg:hidden flex z-100">
       <div className="container ">
-        <ul className="flex items-center justify-between px-5">
-          {navigationMenus.map((item,index) => (
-            <li key={index}>
-              <Link
-                href={item.link}
-                className="flex flex-col gap-y-2 items-center text-gray-600"
-              >
-                {item.icon}
-                <span>{item.title}</span>
-              </Link>
-            </li>
-          ))}
+        <ul className="flex items-center justify-between px-5 text-sm">
+          {navigationMenus.map((item, index) => {
+            const isActive = pathName === item.href;
+            return (
+              <li key={index}>
+                <Link
+                  href={item.href}
+                  className={`flex flex-col gap-y-2 items-center ${isActive ? "text-rose-500" : "text-gray-600"}`}
+                >
+                  <span
+                    className={`px-6 py-2 rounded-full ${isActive ? "bg-rose-100" : ""}`}
+                  >
+                    {cloneElement(item.icon, {
+                      className: isActive ? "fill-rose-500" : "text-gray-600",
+                      size: 20,
+                    })}
+                  </span>
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
