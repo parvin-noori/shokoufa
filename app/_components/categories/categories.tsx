13a57cde9be36d10dda638f6ab8b22f75f7d1433
parvin-noori <parvinnoori0@gmail.com>
2026-08-01 12:58:50 +0330

    update

--- app/_components/categories/categories.tsx
+++ app/_components/categories/categories.tsx
@@ -8,7 +8,7 @@ export default async function Categories() {
       {categories.length && (
         <div className="categories py-10">
           <div className="flex flex-col gap-y-10">
-            <span className="text-2xl mx-auto font-bold">دسته بندی ها</span>
+            <span className="text-2xl mx-auto font-bold .">دسته بندی ها</span>
             <div className="grid lg:grid-cols-5 grid-cols-4 gap-y-10 gap-x-1">
               {categories.map((item, index) => (
                 <Category key={index} category={item} />
