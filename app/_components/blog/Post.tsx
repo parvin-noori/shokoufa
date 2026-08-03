c6968d4615622c908b0d33b6947478cdb890fe11
parvin-noori <parvinnoori0@gmail.com>
2026-08-03 17:39:09 +0330

    fix bug

--- app/_components/blog/Post.tsx
+++ app/_components/blog/Post.tsx
@@ -1,7 +1,7 @@
 import { Clock, Heart } from "lucide-react";
 import Image from "next/image";
-import { PostType } from "./post.types";
 import Link from "next/link";
+import { PostType } from "./post.types";
 
 interface PostProps {
   post: PostType;
@@ -9,22 +9,27 @@ interface PostProps {
 
 export default function Post({ post }: PostProps) {
   return (
-    <Link href="/" className="flex flex-col h-full text-gray-800 gap-y-4 shadow-[0_0_24px_rgba(0,0,0,0.05)] border border-gray-200 p-3 rounded-2xl hover:shadow-[0_0_24px_rgba(0,0,0,0.18)] transition-all duration-200">
+    <Link
+      href="/"
+      className="flex flex-col h-full text-gray-800 gap-y-4 shadow-[0_0_24px_rgba(0,0,0,0.05)] border border-gray-200 p-3 rounded-2xl hover:shadow-[0_0_24px_rgba(0,0,0,0.18)] transition-all duration-200"
+    >
       <Image
-        src={post.image_url}
+        src={post.image_url ?? ""}
         alt={post.title}
         className="w-full rounded-lg"
         width={400}
         height={300}
       />
-      <span className="font-semibold text-lg text-gray-800 line-clamp-2">{post.title}</span>
+      <span className="font-semibold text-lg text-gray-800 line-clamp-2">
+        {post.title}
+      </span>
       <div className="flex items-center gap-x-5 text-gray-400 text-sm mt-auto">
         <span className="flex items-center gap-x-1">
-          <Clock size={16}/>
+          <Clock size={16} />
           16
         </span>
         <span className="flex items-center gap-x-1">
-          <Heart size={16}/>
+          <Heart size={16} />
           999
         </span>
       </div>
