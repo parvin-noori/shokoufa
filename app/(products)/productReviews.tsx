0d07122360352f8aea36f8655a2da17359a5d6c7
parvin-noori <parvinnoori0@gmail.com>
2026-08-23 12:19:42 +0330

    add reviews to product page

--- app/(products)/productReviews.tsx
+++ app/(products)/productReviews.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Rating, Star } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+import { Star as StarRate } from "lucide-react";
 import { useRef } from "react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -15,15 +18,27 @@ interface ProductCarouselProps {
       name: string;
     };
   }[];
+  rate: number;
 }
 
-export default function ProductReviews({ reviews }: ProductCarouselProps) {
+export const ratingStyles = {
+  itemShapes: Star,
+  itemStrokeWidth: 2,
+  activeFillColor: "var(--color-rose-500)",
+  activeStrokeColor: "var(--color-rose-500)",
+  inactiveStrokeColor: "var(--color-rose-500)",
+  inactiveFillColor: "#fff",
+};
+export default function ProductReviews({
+  reviews,
+  rate,
+}: ProductCarouselProps) {
   const prevRef = useRef<HTMLButtonElement>(null);
   const nextRef = useRef<HTMLButtonElement>(null);
   return (
-    <div className="flex flex-col py-10 gap-y-10 rounded-4xl">
+    <div className="flex flex-col  gap-y-10 rounded-4xl h-full">
       <Swiper
-        className="w-full relative  !overflow-visible"
+        className="w-full relative  !overflow-visible h-full"
         // loop={true}
         modules={[Navigation]}
         onBeforeInit={(swiper) => {
@@ -40,30 +55,51 @@ export default function ProductReviews({ reviews }: ProductCarouselProps) {
         breakpoints={{
           320: {
             spaceBetween: 10,
-            slidesPerView: 1.6,
+            slidesPerView: 1.2,
             slidesPerGroup: 1,
           },
           640: {
             spaceBetween: 15,
-            slidesPerView: 2.5,
-            slidesPerGroup: 2,
+            slidesPerView: 1.5,
+            slidesPerGroup: 1,
           },
           1024: {
             spaceBetween: 20,
-            slidesPerView: 3.5,
-            slidesPerGroup: 3,
+            slidesPerView: 2.5,
+            slidesPerGroup: 2,
           },
           1280: {
             spaceBetween: 20,
-            slidesPerView: 4,
-            slidesPerGroup: 4,
+            slidesPerView: 2,
+            slidesPerGroup: 2,
           },
         }}
       >
         {reviews.map((review, index) => (
           <SwiperSlide key={index} className="!h-auto">
-            <span>{review.user.name}</span>
-            <p>{review.comment}</p>
+            <div className="border-2 border-gray-200 rounded-lg p-5 gap-y-5 flex flex-col h-full">
+              <div className="flex items-center justify-between">
+                <span className="font-bold text-xl">{review.user.name}</span>
+                <span className="flex items-center gap-x-2">
+                  {rate}
+                  <StarRate className="lg:hidden flex text-rose-500" size={20}/>
+                  <div className="lg:inline-block hidden">
+                    <Rating
+                      style={{
+                        maxWidth: 90,
+                        direction: "ltr",
+                      }}
+                      value={4.5}
+                      readOnly
+                      itemStyles={ratingStyles}
+                    />
+                  </div>
+                </span>
+              </div>
+              <p className="text-gray-700 text-justify line-clamp-7">
+                {review.comment}
+              </p>
+            </div>
           </SwiperSlide>
         ))}
         <div className="absolute top-1/2 transform -translate-y-1/2 lg:flex hidden items-center justify-between w-full z-10 [&_.swiper-button-disabled]:!opacity-0">
--- app/(products)/products/[slug]/page.tsx
+++ app/(products)/products/[slug]/page.tsx
@@ -1,8 +1,9 @@
 import { getProductBySlug } from "@/app/lib/actions";
+import { Rating } from "@smastrom/react-rating";
 import { ChevronLeft, HandHeart, Leaf, Truck } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import ProductReviews from "../../productReviews";
+import ProductReviews, { ratingStyles } from "../../productReviews";
 
 type Props = {
   params: Promise<{ slug: string }>;
@@ -24,6 +25,14 @@ const services = [
     title: "گل های دستچین شده",
     subtitle: "گل ها توسط گلپرورهای حرفه ای انتخاب شده اند",
   },
+];
+
+const ratings = [
+  { rate: 5, percent: 70 },
+  { rate: 4, percent: 20 },
+  { rate: 3, percent: 90 },
+  { rate: 2, percent: 5 },
+  { rate: 1, percent: 0 },
 ];
 
 export default async function ProductPage({ params }: Props) {
@@ -54,7 +63,10 @@ export default async function ProductPage({ params }: Props) {
             {services.map((service, index) => {
               const Icon = service.icon;
               return (
-                <div className="flex text-center flex-col gap-y-3 items-center" key={index}>
+                <div
+                  className="flex text-center flex-col gap-y-3 items-center"
+                  key={index}
+                >
                   <span className="size-16 rounded-full bg-emerald-50 flex items-center justify-center">
                     <Icon className="text-emerald-600" />
                   </span>
@@ -85,14 +97,55 @@ export default async function ProductPage({ params }: Props) {
               ))}
             </div>
           </div>
-          {product?.reviews?.length ?? (
-            <div className="flex flex-col py-10 gap-y-10">
-              <span className="font-bold text-2xl mx-auto">نظر خریداران</span>
-              <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-3">
-                <ProductReviews reviews={product?.reviews ?? []} />
+          <div className="grid lg:grid-cols-3 grid-cols-1 py-10 gap-x-5 gap-5">
+            <div className="lg:col-span-3  mx-auto">
+              <span className="font-bold text-2xl">نظر خریداران</span>
+            </div>
+            <div className="lg:col-span-1 lg:inline-block hidden">
+              <div className="shadow border-2 border-gray-200 p-5 rounded-lg gap-y-3 flex flex-col">
+                <div className="flex items-center justify-between">
+                  <span className="text-4xl font-bold">{product?.rate}</span>
+                  <Rating
+                    style={{
+                      maxWidth: 120,
+                      direction: "ltr",
+                    }}
+                    value={4.5}
+                    readOnly
+                    itemStyles={ratingStyles}
+                  />
+                </div>
+                <p className="text-gray-700">
+                  براساس {product?.reviews?.length} نظر کاربران
+                </p>
+                <div className="flex flex-col gap-y-2 w-full">
+                  {ratings.map((rate) => (
+                    <div className="flex items-center gap-x-2">
+                      <span className="font-bold">{rate.rate}</span>
+                      <div className="h-2 w-full overflow-hidden rounded-full bg-gray-200">
+                        <div
+                          className="h-full rounded-full bg-rose-500 transition-all"
+                          style={{ width: `${rate.percent}%` }}
+                        />
+                      </div>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
-          )}
+            <div className="lg:col-span-2">
+              {!!product?.reviews?.length && (
+                <div className="flex flex-col h-full">
+                  <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-3 h-full">
+                    <ProductReviews
+                      reviews={product?.reviews ?? []}
+                      rate={product?.rate}
+                    />
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
